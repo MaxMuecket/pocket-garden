037a5d620ea06899f4f98fe728f66f8514d2d107
MaxMuecket <max.muecket@googlemail.com>
2021-06-11 14:19:07 +0200

    🔧 Import globals.css ind preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,8 @@
 import { addDecorator } from '@storybook/react';
 import { MemoryRouter } from 'react-router-dom';
 
+import '../src/app/globals.css';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
