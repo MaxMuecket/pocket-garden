51d1389553c4e2bb353760f7a978f033fff63c71
MaxMuecket <max.muecket@googlemail.com>
2021-06-11 15:01:41 +0200

    ✨ Create Route from Array

---
+++ src/app/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './LandingPage.module.css';
+
+function LandingPage(): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <img src="cover.png" alt=""></img>
+    </div>
+  );
+}
+
+export default LandingPage;
