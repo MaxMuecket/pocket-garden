81969b4532b35845c4814392ca38e851f7a1d700
MaxMuecket <max.muecket@googlemail.com>
2021-06-14 13:59:48 +0200

    💄 Update App.module.css and edit reviews

--- src/app/pages/LandingPage/LandingPage.tsx
+++ src/app/pages/LandingPage/LandingPage.tsx
@@ -4,13 +4,13 @@ import styles from './LandingPage.module.css';
 
 function LandingPage(): JSX.Element {
   return (
-    <header>
-      <div className={styles.container}>
-        <a className={styles.title} href="#">
-          Pocket-<br></br>Garden
-        </a>
-      </div>
-    </header>
+    <div className={styles.container}>
+      <a className={styles.title} href="#">
+        Pocket-
+        <br />
+        Garden
+      </a>
+    </div>
   );
 }
 
