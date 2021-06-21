d9c60f3abb83dbeccc7e2918de4ca4361b2b66f8
MaxMuecket <max.muecket@googlemail.com>
2021-06-21 12:44:05 +0200

    ✨ Add ToggleButton component

---
+++ src/app/components/ToggleSwitch/ToggleSwitch.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import styles from './ToggleSwitch.module.css';
+
+function ToggleSwitch(): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <p>Yes</p>
+      <label className={styles.switch}>
+        <input type="checkbox" />
+        <span className={styles.slider}></span>
+      </label>
+      <p>No</p>
+    </div>
+  );
+}
+
+export default ToggleSwitch;
