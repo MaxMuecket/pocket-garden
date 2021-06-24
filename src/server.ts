a2e066ca2d7a223417cb4551bff6e729eb5e3188
MaxMuecket <max.muecket@googlemail.com>
2021-06-24 12:14:39 +0200

    ✨ Insert, find and delete plants

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,34 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import express from 'express';
 import path from 'path';
+import express from 'express';
+import router from './server/routes';
+import { connectMongoClient } from './utils/database';
 
-const { PORT = 3000 } = process.env;
+const { PORT } = process.env;
 
 const app = express();
 
+app.use(express.json());
+
+app.use('/api', router);
 // Serve storybook production bundle
 app.use('/storybook', express.static('dist/storybook'));
-
 // Serve app production bundle
 app.use(express.static('dist/app'));
-
-app.get('/', (_req, res) => {
-  res.send('Hello World!');
-});
-
 // Handle client routing, return all requests to the app
 app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, 'app/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+if (process.env.MONGODB_URL === undefined) {
+  throw new Error('Missing env MONGODB_URL');
+}
+
+connectMongoClient().then(() => {
+  console.log('Database connected');
+  app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}}`);
+  });
 });
---
+++ src/server/plant.ts
@@ -0,0 +1,24 @@
+import { Collection } from 'mongodb';
+import { NewPlant } from '../types';
+import { getCollection } from '../utils/database';
+
+function getPlantsCollection(): Collection<NewPlant> {
+  return getCollection<NewPlant>('plants');
+}
+
+export async function insertPlant(plant: NewPlant): Promise<NewPlant> {
+  const result = await getPlantsCollection().insertOne(plant, {});
+  const insertedPlant = result.ops[0];
+  return insertedPlant;
+}
+
+export async function findPlant(
+  plant: Partial<NewPlant>
+): Promise<NewPlant | null> {
+  return await getPlantsCollection().findOne(plant);
+}
+
+export async function deletePlant(plant: Partial<NewPlant>): Promise<boolean> {
+  const result = await getPlantsCollection().deleteOne(plant);
+  return typeof result.deletedCount === 'undefined' || result.deletedCount > 0;
+}
