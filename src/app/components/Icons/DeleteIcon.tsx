94c156f266b6587ff5703017f5af07d8165c5c64
MaxMuecket <max.muecket@googlemail.com>
2021-06-25 14:39:50 +0200

    ✨ Add DeleteIcon

---
+++ src/app/components/Icons/DeleteIcon.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function DeleteIcon(): JSX.Element {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="12.414"
+      height="12.414"
+      viewBox="0 0 12.414 12.414"
+    >
+      <g transform="translate(-7108.793 5976.207)">
+        <line
+          x2="11"
+          y2="11"
+          transform="translate(7109.5 -5975.5)"
+          fill="none"
+          stroke="#fff"
+          stroke-width="2"
+        />
+        <line
+          x1="11"
+          y2="11"
+          transform="translate(7109.5 -5975.5)"
+          fill="none"
+          stroke="#fff"
+          stroke-width="2"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default DeleteIcon;
