b8243a05a7217fd2f85e8c6d6f06461039486917
MaxMuecket <max.muecket@googlemail.com>
2021-06-29 08:29:49 +0200

    ✨ Add PageLink component for real

--- src/app/pages/GardenPage/GardenPage.tsx
+++ src/app/pages/GardenPage/GardenPage.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-  Gardening,
-  NewPlant,
-  NewTask,
-  PlantList,
-} from '../../components/PageLink/PageLink.stories';
+import PageLink from '../../components/PageLink/PageLink';
 
 import styles from './GardenPage.module.css';
 
@@ -16,16 +11,24 @@ function GardenPage(): JSX.Element {
       </header>
       <main className={styles.main}>
         <div className={styles.main__newPlant}>
-          <NewPlant />
+          <PageLink pathname="new-plant" variant="newPlant">
+            New<span className={styles.accent}>P</span>lant
+          </PageLink>
         </div>
         <div className={styles.main__plantList}>
-          <PlantList />
+          <PageLink pathname="plant-list" variant="plantList">
+            Plant<span className={styles.accent}>L</span>ist
+          </PageLink>
         </div>
         <div className={styles.main__newTask}>
-          <NewTask />
+          <PageLink pathname="new-task" variant="newTask">
+            New<span className={styles.accent}>T</span>ask
+          </PageLink>
         </div>
         <div className={styles.main__gardening}>
-          <Gardening />
+          <PageLink pathname="gardening" variant="gardening">
+            Gar<span className={styles.accent}>d</span>ening
+          </PageLink>
         </div>
       </main>
     </div>
